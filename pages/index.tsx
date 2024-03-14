a4892ac4c313a991fae1f6155c3c24acbbb78831
LUIS <luis@pop-os.localdomain>
2024-03-13 21:19:57 -0500

    new changes in form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import { useCollection } from "@/realm";
 import useSync from "@/realm/hooks/useSync";
 import { Button, H1 } from "cllk";
-import { useEffect } from "react";
-
 export default function Home() {
+  const collection = useCollection("r", "");
+  const { data } = useSync<{ holi: string }>(
+    collection,
+    ["update"],
+    (set, document) => {
+      document.holi == "user" && set(document);
+    }
+  );
+  console.log(data);
   return (
     <>
       <H1>si</H1>
--- realm/hooks/useSync.tsx
+++ realm/hooks/useSync.tsx
@@ -1,15 +1,35 @@
-function useSync(
-  collection: Realm.Services.MongoDB.MongoDBCollection<any> | undefined
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+function useSync<T>(
+  collection: Realm.Services.MongoDB.MongoDBCollection<any> | undefined,
+  operationType: Array<
+    | "insert"
+    | "delete"
+    | "replace"
+    | "update"
+    | "drop"
+    | "rename"
+    | "dropDatabase"
+    | "invalidate"
+  >,
+  onChange?: (fn: Dispatch<SetStateAction<T>>, document: T) => any
 ) {
+  const [data, setData] = useState<T>();
   const sync = async () => {
     if (collection != undefined) {
       for await (const change of collection?.watch()) {
-        //@ts-ignore
-        return change.fullDocument as any;
+        const bo = operationType.includes(change.operationType);
+        if (bo) {
+          //@ts-ignore
+          onChange?.(setData, change.fullDocument as T);
+        }
       }
     }
   };
-  return { sync };
+  useEffect(() => {
+    sync();
+  }, []);
+  return { data };
 }
 
 export default useSync;
